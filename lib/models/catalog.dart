34427c7dccad28e04a8a5c922cf477ebabab37d7
nerd-sourav <souravprasad1869@gmail.com>
2021-06-02 16:50:21 +0530

    day 10 complete

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -18,11 +18,12 @@ class Item {
 
 final products = [
   Item(
-      id: '001',
-      name: 'iPhone 12 Pro Max',
-      desc: 'iPhone 12th Generation',
-      price: 1299,
-      color: '#33505a',
-      imageUrl:
-          'https://www.citypng.com/public/uploads/preview/-115985227019opakxsfzx.png')
+    id: '001',
+    name: 'iPhone 12 Pro Max',
+    desc: 'iPhone 12th Generation',
+    price: 1299,
+    color: '#33505a',
+    imageUrl:
+        'https://www.citypng.com/public/uploads/preview/-115985227019opakxsfzx.png',
+  )
 ];
